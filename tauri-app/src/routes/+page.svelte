4d5ab1cf993413bef23df0bcc547dd02cfb82077
Kevin Shan <kevinshan2017@gmail.com>
2025-01-30 17:04:52 -0500

    shadcn components

--- tauri-app/src/routes/+page.svelte
+++ tauri-app/src/routes/+page.svelte
@@ -1,3 +1,21 @@
 <script lang="ts">
-    import { db } from '$lib/sqlite'
+    import { onMount } from "svelte";
+    import { db } from "$lib/sqlite";
+	import { goto } from "$app/navigation";
+
+    onMount( async () => {
+        type Table = {
+            name: string
+        }
+
+        const user_data_table: Table = await db.select(`
+            SELECT name 
+            FROM sqlite_master 
+            WHERE type='table' AND name='userData'
+        `);
+
+        if (!user_data_table) {
+            goto('/auth');
+        }
+    })
 </script>
---
+++ tauri-app/src/routes/auth/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { Card } from '$lib/components/ui/card';
+    import { Input } from '$lib/components/ui/input';
+</script>
+
+<div class="flex justify-center items-center h-screen w-screen">
+    <Card>
+        <Input />
+    </Card>
+</div>
